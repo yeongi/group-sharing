3741a46254d730b07be9db9e4aa092bf8fd3ca58
Young37 <20171470@g.dongseo.ac.kr>
2021-12-12 13:26:44 +0900

    회원_그룹 생성기능 추가

--- back/routes/user/user.controller.js
+++ back/routes/user/user.controller.js
@@ -51,4 +51,15 @@ router.post("/group", async (req, res) => {
   }
 });
 
+//그룹에 가입하기 (회원_그룹 생성)
+router.post("/joinGroup", async (req, res) => {
+  try {
+    const result = await UserService.joinGroup(req.body);
+    console.log(result);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
 module.exports = router;
