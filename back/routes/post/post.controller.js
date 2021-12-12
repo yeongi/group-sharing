91ac9c4a0c2e66580e2e6bbcbfdf7373fc0c536b
Young37 <20171470@g.dongseo.ac.kr>
2021-12-12 16:54:42 +0900

    게시글 회원번호,글번호,그룹번호로 하나 조회 기능 추가

--- back/routes/post/post.controller.js
+++ back/routes/post/post.controller.js
@@ -15,6 +15,42 @@ router.get("/", async (req, res) => {
   }
 });
 
+//게시글 번호로 글 하나 가져오기
+router.get("/post_num/:post_num", async (req, res) => {
+  try {
+    //const { post_num } = req.params;
+    const result = await postService.getPostByPostNum(req.params);
+    console.log(result);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
+//그룹 번호로 글 하나 가져오기
+router.get("/grp_num/:grp_num", async (req, res) => {
+  try {
+    //const { grp_num } = req.params;
+    const result = await postService.getPostByPostNum(req.params);
+    console.log(result);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
+//회원번호로 글 하나 가져오기
+router.get("/user_num/:user_num", async (req, res) => {
+  try {
+    //const { user_num } = req.params;
+    const result = await postService.getPostByPostNum(req.params);
+    console.log(result);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
 //게시글 생성
 router.post("/addPost", async (req, res) => {
   try {
--- back/routes/post/post.service.js
+++ back/routes/post/post.service.js
@@ -12,6 +12,42 @@ module.exports = {
       throw error;
     }
   },
+  getPostByPostNum: async (userInfo) => {
+    try {
+      const { post_num } = userInfo;
+      const conn = await pool.getConnection();
+      const query = "SELECT * FROM post WHERE post_num = ?;";
+      const [result] = await conn.query(query,[post_num]);
+      conn.release();
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getPostByGrpNum: async (userInfo) => {
+    try {
+      const { grp_num } = userInfo;
+      const conn = await pool.getConnection();
+      const query = "SELECT * FROM post WHERE grp_num = ?;";
+      const [result] = await conn.query(query,[grp_num]);
+      conn.release();
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getPostByUserNum: async (userInfo) => {
+    try {
+      const { user_num } = userInfo;
+      const conn = await pool.getConnection();
+      const query = "SELECT * FROM post WHERE user_num = ?;";
+      const [result] = await conn.query(query,[user_num]);
+      conn.release();
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
   addPost: async (userInfo) => {
     try {
       const { user_num, grp_num, title, content, open_type, category, file_path } = userInfo;
