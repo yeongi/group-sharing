1bd7b569a7a851cb7219acdc864a8258acbccb62
pjunh1117 <pjunh1117@gmail.com>
2021-12-13 04:42:57 +0900

    post server 로직 전체 확인 및 수정

--- back/routes/post/post.service.js
+++ back/routes/post/post.service.js
@@ -26,8 +26,9 @@ module.exports = {
   getPublicPostByGrpNum: async (grp_num) => {
     try {
       const conn = await pool.getConnection();
-      const query = "SELECT * FROM post WHERE open_type = 'public' AND grp_num = ?;";
-      const [result] = await conn.query(query,[grp_num]);
+      const query =
+        "SELECT * FROM post WHERE open_type = 'public' AND grp_num = ?;";
+      const [result] = await conn.query(query, [grp_num]);
       conn.release();
       return result;
     } catch (error) {
@@ -39,7 +40,7 @@ module.exports = {
       const { post_num } = userInfo;
       const conn = await pool.getConnection();
       const query = "SELECT * FROM post WHERE post_num = ?;";
-      const [result] = await conn.query(query,[post_num]);
+      const [result] = await conn.query(query, [post_num]);
       conn.release();
       return result;
     } catch (error) {
@@ -51,7 +52,7 @@ module.exports = {
       const { grp_num } = userInfo;
       const conn = await pool.getConnection();
       const query = "SELECT * FROM post WHERE grp_num = ?;";
-      const [result] = await conn.query(query,[grp_num]);
+      const [result] = await conn.query(query, [grp_num]);
       conn.release();
       return result;
     } catch (error) {
@@ -61,21 +62,32 @@ module.exports = {
   getShared: async (grp_num) => {
     try {
       const conn = await pool.getConnection();
-      const query = "SELECT * FROM post WHERE grp_num IN (SELECT grp_num2 FROM group_sharing WHERE grp_num = ?) AND open_type = 'protected'";
-      const [result] = await conn.query(query,[grp_num]);
+      const query =
+        "SELECT * FROM post WHERE grp_num IN (SELECT grp_num2 FROM group_sharing WHERE grp_num = ?) AND open_type = 'protected'";
+      const [result] = await conn.query(query, [grp_num]);
       conn.release();
       return result;
     } catch (error) {
       throw error;
     }
   },
-
+  getPostByUserNum: async (user_num) => {
+    try {
+      const conn = await pool.getConnection();
+      const query = "SELECT * FROM post WHERE user_num = ?;";
+      const [result] = await conn.query(query, [user_num]);
+      conn.release();
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
   getPostByUserNumAndGroupNum: async (userInfo) => {
     try {
-      const { user_num,group_num } = userInfo;
+      const { user_num, grp_num } = userInfo;
       const conn = await pool.getConnection();
-      const query = "SELECT * FROM post WHERE user_num = ? AND group_num = ?;";
-      const [result] = await conn.query(query,[user_num,group_num]);
+      const query = "SELECT * FROM post WHERE user_num = ? AND grp_num = ?;";
+      const [result] = await conn.query(query, [user_num, grp_num]);
       conn.release();
       return result;
     } catch (error) {
@@ -84,14 +96,21 @@ module.exports = {
   },
   addPost: async (userInfo) => {
     try {
-      const { user_num, grp_num, title, content, open_type, category, file_path } = userInfo;
+      const { user_num, grp_num, title, content, open_type } = userInfo;
       const conn = await pool.getConnection();
-      const query = "INSERT INTO `post` (user_num, grp_num, title, content, open_type, category, file_path) VALUES (?,?,?,?,?,?,?);";
-      const [result] = await conn.query(query,[user_num, grp_num, title, content, open_type, category, file_path]);
+      const query =
+        "INSERT INTO `post` (user_num, grp_num, title, content, open_type, category, file_path) VALUES (?,?,?,?,?,'글','null');";
+      const [result] = await conn.query(query, [
+        user_num,
+        grp_num,
+        title,
+        content,
+        open_type,
+      ]);
       conn.release();
       return result;
     } catch (error) {
       throw error;
     }
-  }
+  },
 };
