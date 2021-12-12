80e5c019097cc30fddd7e21de2993d5eb6025fd8
pjunh1117 <pjunh1117@gmail.com>
2021-12-13 04:01:00 +0900

    Group API 생성

--- back/routes/post/post.controller.js
+++ back/routes/post/post.controller.js
@@ -7,7 +7,7 @@ const postService = require("./post.service");
 //게시글 전부 가져오기
 router.get("/", async (req, res) => {
   try {
-    const result = await postService.getAllPost();//파라미터 필요 없음
+    const result = await postService.getAllPost(); //파라미터 필요 없음
     console.log(result);
     res.status(200).json({ status: 200, data: result, message: "Success" });
   } catch (error) {
@@ -18,7 +18,7 @@ router.get("/", async (req, res) => {
 //공개 게시글만 전부 조회
 router.get("/public", async (req, res) => {
   try {
-    const result = await postService.getAllPublicPost();//파라미터 필요 없음
+    const result = await postService.getAllPublicPost(); //파라미터 필요 없음
     console.log(result);
     res.status(200).json({ status: 200, data: result, message: "Success" });
   } catch (error) {
@@ -74,7 +74,6 @@ router.get("/grp_num/:grp_num", async (req, res) => {
   }
 });
 
-
 //회원번호로 글 가져오기
 router.get("/user_num/:user_num", async (req, res) => {
   try {
@@ -86,7 +85,6 @@ router.get("/user_num/:user_num", async (req, res) => {
     return res.status(500).json({ status: 500, message: error });
   }
 });
-
 
 //회원번호와 그룹번호로 해당 그룹에 대한 나의 게시글 조회
 router.get("/user_num/:user_num/group_num/:group_num", async (req, res) => {
@@ -103,7 +101,7 @@ router.get("/user_num/:user_num/group_num/:group_num", async (req, res) => {
 //게시글 생성
 router.post("/addPost", async (req, res) => {
   try {
-     //const { user_num, grp_num, title, content, open_type, category, file_path } = req.body;
+    //const { user_num, grp_num, title, content, open_type, category, file_path } = req.body;
     const result = await postService.addPost(req.body);
     console.log(result);
     res.status(200).json({ status: 200, data: result, message: "Success" });
@@ -111,6 +109,5 @@ router.post("/addPost", async (req, res) => {
     return res.status(500).json({ status: 500, message: error });
   }
 });
-
 
 module.exports = router;
