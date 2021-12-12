da570f5a094ad7f7d86cadb3c5eba127e3ba974b
Young37 <20171470@g.dongseo.ac.kr>
2021-12-12 16:34:18 +0900

    회원의 관심사 등록하는 기능 추가

--- back/routes/interest/interest.controller.js
+++ back/routes/interest/interest.controller.js
@@ -13,4 +13,16 @@ router.get("/", async (req, res) => {
   }
 });
 
+//관심사_회원 등록하기
+router.get("/addInterestUser/userNum/:user_num/interestNum/:interest_num", async (req, res) => {
+  try {
+    //const { user_num,interest_num } = req.params;
+    const result = await interestService.addInterestUser(req.params);
+    console.log(result);
+    res.status(200).json({ status: 200, data: result, message: "Success" });
+  } catch (error) {
+    return res.status(500).json({ status: 500, message: error });
+  }
+});
+
 module.exports = router;
--- back/routes/interest/interest.service.js
+++ back/routes/interest/interest.service.js
@@ -11,6 +11,19 @@ module.exports = {
     } catch (error) {
       throw error;
     }
+  },
+  addInterestUser: async (userInfo) => {
+    try {
+      const { user_num,interest_num } = userInfo;
+      const conn = await pool.getConnection();
+
+      const query = "INSERT INTO interest_user (user_num,interest_num) VALUES (?,?);";
+      const [result] = await conn.query(query,[user_num,interest_num]);
+      conn.release();
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 };
 
