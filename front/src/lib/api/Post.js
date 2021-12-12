b17e177ac22bd0890051a06b8485c0b7a3c89f6f
pjunh1117 <pjunh1117@gmail.com>
2021-12-13 03:50:07 +0900

    Post API 생성

--- front/src/lib/api/Post.js
+++ front/src/lib/api/Post.js
@@ -1,19 +1,79 @@
 const CREATE_API = "http://localhost:4000/posts";
 
 const PostAPI = {
-  getGrpPublicList: (num) => {
-    //name, id, password
-    return fetch(`${CREATE_API}/public/grpNum/${num}`, {
+  getAllPost: () => {
+    return fetch(`${CREATE_API}/`, {
       method: "get",
       headers: {
         "Content-type": "application/json",
       },
     });
   },
-
+  getPublicPost: () => {
+    return fetch(`${CREATE_API}/public`, {
+      method: "get",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  },
+  getGrpPublicList: (grp_num) => {
+    //name, id, password
+    return fetch(`${CREATE_API}/public/grpNum/${grp_num}`, {
+      method: "get",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  },
+  getOnePost: (post_num) => {
+    //name, id, password
+    return fetch(`${CREATE_API}/post_num/${post_num}`, {
+      method: "get",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  },
+  getGrpNumPost: (grp_num) => {
+    //name, id, password
+    return fetch(`${CREATE_API}/grp_num/${grp_num}`, {
+      method: "get",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  },
+  getProtectedPost: (grp_num) => {
+    //name, id, password
+    return fetch(`${CREATE_API}/grp_num/${grp_num}`, {
+      method: "get",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  },
+  getUserNumPost: (user_num) => {
+    //name, id, password
+    return fetch(`${CREATE_API}/user_num/${user_num}`, {
+      method: "get",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  },
+  getMyPost: (user_num, group_num) => {
+    //name, id, password
+    return fetch(`${CREATE_API}/user_num/${user_num}/group_num/${group_num}`, {
+      method: "get",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  },
   postInterest: (body) => {
     //main_interest,sub_interest
-    return fetch(`${CREATE_API}/user`, {
+    return fetch(`${CREATE_API}/addPost`, {
       method: "post",
       headers: {
         "Content-type": "application/json",
